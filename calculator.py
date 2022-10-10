f4864848f12cd0fc203fe6b83fcae6cc47385d24
WesLemon <wesleyshaw25@gmail.com>
2022-10-10 17:38:16 +0100

    validity_check works for all valid inputs except those including negative integers

--- calculator.py
+++ calculator.py
@@ -7,15 +7,32 @@ operators = ['+', '-', '*']
 
 # Function that checks whether the string entered by the user is valid.
 def check_validity(string):
-    # Checks if there's an operators at the beginning or the end of the string.
+    # Checks if there's an operator at the beginning or the end of the string.
     if string[0] in operators or string[len(string) - 1] in operators:
         return False
 
+    # Boolean value for error checking
+    operator_last = False
     for i in range(len(string)):
-        # Checks to see if each character is a space, digit or an operator.
         if string[i] == ' ':
+            # Loop that checks for double operators/operands separated by a space character
+            for (j) in range(i+1, len(string)):
+                if string[j] in digits:
+                    if not operator_last:
+                        return False
+                    else:
+                        break
+                elif string[j] in operators:
+                    if operator_last:
+                        return False
+                    else:
+                        break
+                elif string[j] == ' ':
+                    continue
             continue
+        operator_last = False
         if string[i] not in digits:
+            operator_last = True
             if string[i] not in operators:
                 return False
             # Check for double operators.
