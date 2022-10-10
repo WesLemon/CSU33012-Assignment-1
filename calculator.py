74af3db07d68ff77a21773831a4b4970d398cb07
WesLemon <wesleyshaw25@gmail.com>
2022-10-10 22:19:20 +0100

    linked gui with logic functions

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+from queue import Queue
 import calculator_gui
 
 digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -64,9 +65,66 @@ def convert_string_to_list(user_input):
     return new_string.split()
 
 
-def calculate():
-    pass
-    # TODO
+# Function that implements basic calculator Logic
+# parameters: toCalculate = A string of infixed valid operators and operands
+def calculate(toCalculate):
+    # Initialise a queue to evaluate the equation
+    q = Queue(maxsize=0)
+
+    # loop through the list and push operands and operators into a queue
+    #   while evaluating multiplication operators
+    i = 0
+    while i < len(toCalculate):
+        # if the element is an operator put it on the queue
+        if toCalculate[i] in operators:
+            q.put(toCalculate[i])
+
+        # else it must be an operand so cast it as an int and put it on the stack
+        else:
+            swapped = False
+            # if there is more than 1 character in the list
+            if i + 1 < len(toCalculate):
+                # if the next operator is multiplication evaluate it and put in on result on the queue
+                #   and skip over the operators and operands used
+                if toCalculate[i + 1] == '*':
+                    q.put(int(toCalculate[i]) * int(toCalculate[i + 2]))
+                    i = i + 2
+                    swapped = True
+
+            # if no multiplication was found
+            if not swapped:
+                q.put(int(toCalculate[i]))
+
+        i += 1
+
+    # initialise the result
+    result = 0
+
+    while not q.empty():
+        # while the FIFO queue isn't empty get the first elements which will be
+        #   either an operand or an operator
+        element = q.get()
+
+        # Checking for single element queues
+        if not q.empty():
+            # Dealing with the 2 other possible scenarios, an operator or an operand dequeued
+            #   if the operator is a + add the operands, else it must be a minus so subtract them
+            #   and add them to the result
+            element2 = q.get()
+            if element in operators:
+                if element == "+":
+                    result += element2
+                else:
+                    result -= element2
+            else:
+                element3 = q.get()
+                if element2 == "+":
+                    result += (element + element3)
+                else:
+                    result += (element - element3)
+        else:
+            result = element
+    return result
 
 
 def main():
--- calculator_gui.py
+++ calculator_gui.py
@@ -1,7 +1,6 @@
 import tkinter as tk
-import calculator
-
-calculation = ""
+import calculator as calc
+toCalculate = ""
 
 
 class GUI:
@@ -15,25 +14,25 @@ class GUI:
         self.text_result.grid(columnspan=5)
 
         # Individual buttons for the calculator
-        btn_1 = tk.Button(self.root, text="1", command=lambda: self.add_to_calculation(1), width=5, font=("Arial", 14))
+        btn_1 = tk.Button(self.root, text="1", command=lambda: self.add_to_calculation("1"), width=5, font=("Arial", 14))
         btn_1.grid(row=2, column=1)
-        btn_2 = tk.Button(self.root, text="2", command=lambda: self.add_to_calculation(2), width=5, font=("Arial", 14))
+        btn_2 = tk.Button(self.root, text="2", command=lambda: self.add_to_calculation("2"), width=5, font=("Arial", 14))
         btn_2.grid(row=2, column=2)
-        btn_3 = tk.Button(self.root, text="3", command=lambda: self.add_to_calculation(3), width=5, font=("Arial", 14))
+        btn_3 = tk.Button(self.root, text="3", command=lambda: self.add_to_calculation("3"), width=5, font=("Arial", 14))
         btn_3.grid(row=2, column=3)
-        btn_4 = tk.Button(self.root, text="4", command=lambda: self.add_to_calculation(4), width=5, font=("Arial", 14))
+        btn_4 = tk.Button(self.root, text="4", command=lambda: self.add_to_calculation("4"), width=5, font=("Arial", 14))
         btn_4.grid(row=3, column=1)
-        btn_5 = tk.Button(self.root, text="5", command=lambda: self.add_to_calculation(5), width=5, font=("Arial", 14))
+        btn_5 = tk.Button(self.root, text="5", command=lambda: self.add_to_calculation("5"), width=5, font=("Arial", 14))
         btn_5.grid(row=3, column=2)
-        btn_6 = tk.Button(self.root, text="6", command=lambda: self.add_to_calculation(6), width=5, font=("Arial", 14))
+        btn_6 = tk.Button(self.root, text="6", command=lambda: self.add_to_calculation("6"), width=5, font=("Arial", 14))
         btn_6.grid(row=3, column=3)
-        btn_7 = tk.Button(self.root, text="7", command=lambda: self.add_to_calculation(7), width=5, font=("Arial", 14))
+        btn_7 = tk.Button(self.root, text="7", command=lambda: self.add_to_calculation("7"), width=5, font=("Arial", 14))
         btn_7.grid(row=4, column=1)
-        btn_8 = tk.Button(self.root, text="8", command=lambda: self.add_to_calculation(8), width=5, font=("Arial", 14))
+        btn_8 = tk.Button(self.root, text="8", command=lambda: self.add_to_calculation("8"), width=5, font=("Arial", 14))
         btn_8.grid(row=4, column=2)
-        btn_9 = tk.Button(self.root, text="9", command=lambda: self.add_to_calculation(9), width=5, font=("Arial", 14))
+        btn_9 = tk.Button(self.root, text="9", command=lambda: self.add_to_calculation("9"), width=5, font=("Arial", 14))
         btn_9.grid(row=4, column=3)
-        btn_0 = tk.Button(self.root, text="0", command=lambda: self.add_to_calculation(0), width=5, font=("Arial", 14))
+        btn_0 = tk.Button(self.root, text="0", command=lambda: self.add_to_calculation("0"), width=5, font=("Arial", 14))
         btn_0.grid(row=5, column=2)
         btn_plus = tk.Button(self.root, text="+", command=lambda: self.add_to_calculation("+"), width=5,
                              font=("Arial", 14))
@@ -44,7 +43,7 @@ class GUI:
         btn_mul = tk.Button(self.root, text="*", command=lambda: self.add_to_calculation('*'), width=5,
                             font=("Arial", 14))
         btn_mul.grid(row=4, column=4)
-        btn_equals = tk.Button(self.root, text="=", command=calculator.calculate, width=11,
+        btn_equals = tk.Button(self.root, text="=", command=self.logic, width=11,
                                font=("Arial", 14))
         btn_equals.grid(row=5, column=3, columnspan=2)
         btn_clear = tk.Button(self.root, text="C", command=self.clear_field, width=5, font=("Arial", 14))
@@ -53,18 +52,30 @@ class GUI:
 
     # Clears the text field in the calculator
     def clear_field(self):
-        global calculation
-        calculation = ""
+        global toCalculate
+        toCalculate = ""
         self.text_result.delete(1.0, "end")
-
-    # Calculates the equation from the user's input string
-    def calculate(self):
-        global calculation
-        # TODO
 
     # Adds the value associated with each button to the user's input string
     def add_to_calculation(self, symbol):
-        global calculation
-        calculation += str(symbol)
+        global toCalculate
+        toCalculate += str(symbol)
         self.text_result.delete(1.0, "end")
-        self.text_result.insert(1.0, calculation)
+        self.text_result.insert(1.0, toCalculate)
+
+    def logic(self):
+        global toCalculate
+        expression = calc.convert_string_to_list(toCalculate)
+        if expression is None:
+            self.clear_field()
+            self.text_result.insert(1.0, 'Error: Invalid String')
+            toCalculate = ""
+        elif calc.check_validity(expression):
+            result = calc.calculate(expression)
+            self.text_result.delete(1.0, "end")
+            self.text_result.insert(1.0, result)
+            toCalculate = str(result)
+        else:
+            self.clear_field()
+            self.text_result.insert(1.0, 'Error: Invalid String')
+            toCalculate = ""
